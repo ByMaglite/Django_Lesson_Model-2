3237496b3b0b6466d6ab7258534bbd3d408e30c6
ByMaglite <fceyhan55@gmail.com>
2021-10-12 19:08:28 +0300

    table updated

--- fscohort/models.py
+++ fscohort/models.py
@@ -5,7 +5,14 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField()
+    about_student = models.TextField(null=True, blank =True )
+    image = models.ImageField(null = True, blank=True)
     
     
     def __str__(self):
         return(f"{self.number} - {self.first_name} {self.last_name}")
+    
+    class Meta:
+        ordering = ["number"]
+        verbose_name_plural = "Student_List"
+        db_table = "Student_Table"
