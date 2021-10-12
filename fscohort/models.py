bdaad16a68ad33e462faff150547699771f1da01
ByMaglite <fceyhan55@gmail.com>
2021-10-12 17:37:48 +0300

    v1

--- fscohort/models.py
+++ fscohort/models.py
@@ -8,4 +8,4 @@ class Student(models.Model):
     
     
     def __str__(self):
-        return f("{self.number}-{self.first_name}- {self.last_name}")
+        return(f"{self.number} - {self.first_name} {self.last_name}")
